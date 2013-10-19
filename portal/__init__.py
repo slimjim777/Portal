a64fce44184e595d7715af5abb5bf3ae90da5189
James Jesudason <james.jesudason@gmail.com>
2013-10-19 11:00:36 +0100

    Force SSL for connections

--- portal/__init__.py
+++ portal/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_sslify import SSLify
 import os
 
 app = Flask(__name__)
+sslify = SSLify(app, permanent=True)
 
-app.debug = True
+app.debug = False
 app.secret_key = os.environ["SECRET_KEY"]
 
 import portal.views
